48286df92f875fb32e007a98837ab486af793936
Hayden Beames <90889247+haydenbeames@users.noreply.github.com>
2023-06-29 13:21:34 -0700

    Update c_4to2.sv

--- c_4to2.sv
+++ c_4to2.sv
@@ -22,13 +22,5 @@ module compressor_4_2(
   assign mux0 = ~(cin | in1);
   assign c = (i2_i3_i4)?~mux1:~mux0; 
   assign cout = ~((~(in2 & in3)) & (~(in2 & in4)) & (~(in3 & in4)));  
-  //assign s = sPartial ^ cin;
-  //assign c = (sPartial==0)?in1:cin;
-  //assign cout = (partial12==1)?in3:in1;
-  //wire s1;
-  
-  //full_adder fa1(in1, in2, in3, s1, cout);
-  //full_adder fa2(cin, in4, s1, s, c);  
-
   
 endmodule
